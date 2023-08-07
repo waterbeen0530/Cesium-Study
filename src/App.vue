3e01b3559760335ff5990a7f68e4d6e51ac714a3
subinsu <hange253@gmail.com>
2023-08-07 16:38:45 +0900

    [INIT]
    directory 구조 setting

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><MainVue /></div>
+</template>
+
+<script>
+import MainVue from "./views/main.vue";
+export default {
+  name: "App",
+  components: {
+    MainVue,
+  },
+};
+</script>
+<style></style>;
---
+++ src/views/main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><titleItemVue /></div>
+</template>
+
+<script>
+import titleItemVue from "@/components/main/titleItem.vue";
+export default {
+  name: "MainVue",
+  components: {
+    titleItemVue,
+  },
+};
+</script>
