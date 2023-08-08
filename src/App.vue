316d1f099c22e1b775faffbac29f1c3eeb920748
subinsu <hange253@gmail.com>
2023-08-08 10:48:50 +0900

    [CREATE] header
    헤더 생성

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div><MainVue /></div>
+  <div class="rute">
+    <HeaderVue />
+    <MainVue />
+  </div>
 </template>
 
 <script>
+import HeaderVue from "./components/common/header.vue";
 import MainVue from "./views/main.vue";
+
 export default {
   name: "App",
   components: {
+    HeaderVue,
     MainVue,
   },
 };
 </script>
-<style></style>;
+<style>
+.rute {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>;
---
+++ src/components/common/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <h1 id="logo">Cesium-Study</h1>
+      <div class="navBox">
+        <p class="text">Beginner</p>
+        <p class="text">Geometries</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderVue",
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+}
+
+.wrapper {
+  gap: 60px;
+  width: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+#logo {
+  font-size: 30px;
+  cursor: pointer;
+}
+
+.navBox {
+  gap: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.text {
+  font-size: 18px;
+  font-weight: 500;
+  color: #222;
+  cursor: pointer;
+}
+</style>
