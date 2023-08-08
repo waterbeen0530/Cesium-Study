9c9561971ce7183c91d17649a32501c9e9b1784a
subinsu <hange253@gmail.com>
2023-08-08 10:49:38 +0900

    [CREATE] main
    banner 생성

---
+++ src/components/main/banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="textBox">
+      <h1 id="title">Cesium 공부 모음</h1>
+      <p id="text">
+        3D지도 오픈소스 JS 라이브러리 cesiumJS.<br />제가 공부한 것들 모음
+        입니다.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerVue",
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  width: 100%;
+  height: 400px;
+  border-radius: 10px;
+  background: linear-gradient(
+    90deg,
+    rgba(96, 178, 206, 1) 0%,
+    rgba(101, 136, 241, 1) 51%,
+    rgba(140, 142, 235, 1) 100%
+  );
+}
+
+.textBox {
+  position: absolute;
+  top: 100px;
+  left: 60px;
+  color: #fff;
+}
+
+#text {
+  line-height: 26px;
+}
+</style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,13 +1,23 @@
 <template>
-  <div><titleItemVue /></div>
+  <div class="container"><BannerVue /></div>
 </template>
 
 <script>
-import titleItemVue from "@/components/main/titleItem.vue";
+import BannerVue from "@/components/main/banner.vue";
 export default {
   name: "MainVue",
   components: {
-    titleItemVue,
+    BannerVue,
   },
 };
 </script>
+
+<style scoped>
+.container {
+  width: 1000px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
